1834d524ffaa0a6ac579909bc673e3ead9ac6e1a
Civetdelapin <bababapt@gmail.com>
2019-04-16 22:13:31 +0200

    animations working for every components

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,9 +1,15 @@
-import {Component, OnInit, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ChangeDetectorRef,
+  ViewChild,
+  Renderer2
+} from '@angular/core';
 
 import {
-  bounceInOnEnterAnimation, fadeInLeftOnEnterAnimation,
-  fadeInOnEnterAnimation, fadeInRightOnEnterAnimation,
-  fadeInUpOnEnterAnimation, rotateInDownRightOnEnterAnimation
+  bounceInOnEnterAnimation, fadeInLeftAnimation,
+  fadeInOnEnterAnimation, fadeInRightAnimation, fadeInUpAnimation, rotateInDownRightOnEnterAnimation
 } from 'angular-animations';
 
 import { ParticleParams } from "./particleParams";
@@ -13,6 +19,7 @@ import { flipInY} from 'ng-animate';
 
 import { ProjectList } from "../projectList";
 import {TranslateService} from "@ngx-translate/core";
+import {InViewportConfigOptions} from "ng-in-viewport";
 
 @Component({
   selector: 'app-home',
@@ -21,66 +28,69 @@ import {TranslateService} from "@ngx-translate/core";
   animations: [
     fadeInOnEnterAnimation({ delay : 0 }),
     bounceInOnEnterAnimation({ delay : 400 }),
-    fadeInUpOnEnterAnimation({ anchor: 'fadeInUpOnEnter'}),
-    fadeInLeftOnEnterAnimation({ anchor: 'fadeInLeftOnEnter'}),
-    fadeInRightOnEnterAnimation({anchor : 'fadeInRightOnEnter'}),
+    fadeInUpAnimation({ anchor: 'fadeInUp'}),
+    fadeInLeftAnimation({ anchor: 'fadeInLeft'}),
+    fadeInRightAnimation({anchor : 'fadeInRight'}),
     rotateInDownRightOnEnterAnimation({anchor : 'rotateInDownRightOnEnter'})
   ]
 })
 export class HomeComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('mainDiv') mainDiv;
+
   showHeaderText : boolean = false;
-  enterAnimation: TextAnimation = {
+
+  enterTextAnimation: TextAnimation = {
     animation: flipInY,
     delay: 50,
     type: 'letter'
   };
 
-  myStyle: object = {};
-  myParams: object = {};
-  width: number = 100;
-  height: number = 100;
+  myParticleStyle: object = {
+    'position': 'absolute',
+    'width': '100%',
+    'height': '100%',
+    'top': 0,
+    'left': 0,
+    'right': 0,
+    'bottom': 0,
+  };
+  myParticleParams: object = ParticleParams;
 
-  projectList : any;
+  projectList : any = ProjectList;
+
+  inViewportOptions: InViewportConfigOptions;
 
   sectionList = ['about', 'work', 'skills', 'contact'];
   activeSections = [];
 
-  constructor(public translate: TranslateService) {
+  constructor(public translate: TranslateService,
+              private changeDetector: ChangeDetectorRef,
+              private renderer : Renderer2
+              ) {
   }
 
   ngOnInit() {
-
-    this.projectList = ProjectList;
-
-    this.myStyle = {
-      'position': 'absolute',
-      'width': '100%',
-      'height': '100%',
-      'top': 0,
-      'left': 0,
-      'right': 0,
-      'bottom': 0,
-    };
-    this.myParams = ParticleParams;
-  };
+    this.inViewportOptions = {
+      threshold: Array(101).fill(null).map((v, k) => k / 100)
+    }
+  }
 
   ngAfterViewInit() {
+
     const _this = this;
-
-
     setTimeout(function(){
       _this.showHeaderText = true;
     },1000);
 
-
+    this.changeDetector.detectChanges();
   }
 
   public onIntersection({ target, visible }: { target: Element; visible: boolean }, divId : string): void {
     if(visible && this.activeSections.indexOf(divId) === -1){
       let position = this.sectionList.indexOf(divId);
       for(let i = 0; i <= position;  i++){
-        this.activeSections.push(divId);
+        this.activeSections.push(this.sectionList[i]);
       }
     }
   }
@@ -91,5 +101,12 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   getSubHeader() : string {
     return this.translate.instant('sub-header')
+  }
+
+  setOpacity(element, section: string){
+    console.log(element);
+    if(this.isSectionActive(section)){
+      this.renderer.setStyle(element, 'opacity', 1);
+    }
   }
 }
